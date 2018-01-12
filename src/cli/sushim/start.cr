134c08295eb48f138a945c4039c7efdd12f5ed9e
tbrand <taichiro0709@gmail.com>
2018-01-13 00:52:12 +0900

    wip

--- src/cli/sushim/start.cr
+++ src/cli/sushim/start.cr
@@ -1,7 +1,8 @@
 module ::Sushi::Interface
   class Start < CLI
     @wallet_path : String?
-    @node : String?
+    @node        : String?
+    @is_testnet  : Bool = false
 
     def sub_actions
       [] of SushiAction
@@ -14,6 +15,9 @@ module ::Sushi::Interface
         }
         parser.on("-n NODE", "--node=NODE", "Connecting node") { |node|
           @node = node
+        }
+        parser.on("--testnet", "Set network type as testnet (default is mainnet)") {
+          @is_testnet = true
         }
       end
     end
@@ -37,7 +41,12 @@ module ::Sushi::Interface
         puts_help(HELP_CONNECTING_NODE)
       end
 
-      miner = Core::Miner.new(host, port, wallet_path)
+      wallet = Core::Wallet.from_path(wallet_path)
+      wallet_is_testnet = (Core::Wallet.address_network_type(wallet.address)[:name] == "testnet")
+
+      raise "Wallet type mismatch" if @is_testnet != wallet_is_testnet
+
+      miner = Core::Miner.new(@is_testnet, host, port, wallet)
       miner.run
     end
   end
