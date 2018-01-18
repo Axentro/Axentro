1474ba949f9fb49f476c7e7ef654e3a47066be26
tbrand <taichiro0709@gmail.com>
2018-01-18 18:06:58 +0900

    fix

--- src/core/blockchain/consensus.cr
+++ src/core/blockchain/consensus.cr
@@ -4,7 +4,7 @@ module ::Sushi::Core::Consensus
   MINER_DIFFICULTY = 3
 
   # SHA256 Implementation
-  def valid?(block_hash : String, nonce : UInt64, difficulty : Int32 = DIFFICULTY) : Bool
+  def _valid?(block_hash : String, nonce : UInt64, difficulty : Int32 = DIFFICULTY) : Bool
     guess_nonce = "#{block_hash}#{nonce}"
     guess_hash = sha256(guess_nonce)
     guess_hash[0, difficulty] == "0" * difficulty
@@ -16,13 +16,24 @@ module ::Sushi::Core::Consensus
   K = 512
 
   # Scrypt Implementation
-  # def valid?(block_hash : String, nonce : UInt64, difficulty : Int32 = DIFFICULTY) : Bool
-  #   nonce_salt = nonce.to_s(16)
-  #   nonce_salt = "0" + nonce_salt if nonce_salt.bytesize%2 != 0
-  #  
-  #   hash = ::Scrypt::Engine.crypto_scrypt(block_hash, nonce.to_s(16), N, R, P, K)
-  #   hash[0, difficulty] == "0" * difficulty
-  # end
+  def valid?(block_hash : String, nonce : UInt64, difficulty : Int32 = DIFFICULTY) : Bool
+    nonce_salt = nonce.to_s(16)
+    nonce_salt = "0" + nonce_salt if nonce_salt.bytesize%2 != 0
+
+    # todo: use libscrypt directory
+    # slice = Slice(UInt8).new(3)
+    # slice[0] = 0_u8
+    # slice[1] = 0_u8
+    # slice[2] = 0_u8
+    #  
+    # buffer = Slice(UInt8).new(K)
+    #  
+    # res = LibScrypt.crypto_scrypt("abc", 3, slice, 3, N, R, P, buffer, K)
+    # p res
+
+    hash = ::Scrypt::Engine.crypto_scrypt(block_hash, nonce.to_s(16), N, R, P, K)
+    hash[0, difficulty] == "0" * difficulty
+  end
 
   include Hashes
 end
