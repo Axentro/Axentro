fe1ec566844c2179ae37543616b9c04df4334eef
tbrand <taichiro0709@gmail.com>
2018-01-24 00:04:43 +0900

    fix

---
+++ spec/integration/utils/node.cr
@@ -0,0 +1,30 @@
+module ::Integration::Utils::Node
+
+  def sushid(args) : String
+    _args = args
+            .map { |arg| arg.to_s }
+            .join(" ")
+
+    bin = File.expand_path("../../../../bin/sushid", __FILE__)
+
+    "#{bin} #{_args}"
+  end
+
+  def node(port : Int32, is_private : Bool, connect_port : Int32?, num : Int32)
+    args = ["-p", port, "-w", wallet(num), "--testnet"]
+    args << "-n http://127.0.0.1:#{connect_port}" if connect_port
+
+    if is_private
+      args << "--private"
+    else
+      args << "-u"
+      args << "http://127.0.0.1:#{port}"
+    end
+
+    bin = sushid(args)
+
+    spawn do
+      system("#{bin} &> #{log_path(num)}")
+    end
+  end
+end
---
+++ spec/integration/utils/wallet.cr
@@ -0,0 +1,5 @@
+module ::Integration::Utils::Wallet
+  def wallet(num : Int32) : String
+    File.expand_path("../../../../wallets/testnet-#{num}.json", __FILE__)
+  end
+end
