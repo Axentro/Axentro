4e44b6fd48013cb59109bfd39d2ef12cdbd40151
tbrand <taichiro0709@gmail.com>
2018-01-25 22:35:44 +0900

    fix

--- spec/integration/spec.cr
+++ spec/integration/spec.cr
@@ -4,11 +4,11 @@ describe Integration do
 
   it "the integration test" do
 
-    unless ARGV.includes?("--local")
+    if ENV.has_key?("TRAVIS")
       runner = ::Integration::Runner.new
       runner.run!.should be_true
     else
-      puts "skip integration test."
+      STDERR.puts "skip integration test."
     end
   end
 end
