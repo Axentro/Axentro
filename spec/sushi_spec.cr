f6515d21ff112d2863a5ccc21e88b892fe6b309d
tbrand <taichiro0709@gmail.com>
2018-01-28 14:00:18 +0900

    integration test

--- spec/sushi_spec.cr
+++ spec/sushi_spec.cr
@@ -6,13 +6,13 @@ require "./units/*"
 puts "\n----- Integration test -----"
 require "./integration/spec"
 
-# unless ARGV.includes?("--local")
-#   runner = ::Integration::Runner.new
-#   runner.run!
-#
-#   sleep 10
-#
-#   runner.fin
-# else
-#   puts "skip integration test."
-# end
+unless ARGV.includes?("--local")
+  runner = ::Integration::Runner.new
+  runner.run!
+
+  sleep 10
+
+  runner.fin
+else
+  puts "skip integration test."
+end
