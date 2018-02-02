752efab29b38ff560f4b71b079e283da96b1329f
tbrand <taichiro0709@gmail.com>
2018-02-03 00:50:12 +0900

    fix

--- src/core/modules/hashes.cr
+++ src/core/modules/hashes.cr
@@ -1,11 +1,11 @@
 module ::Sushi::Core::Hashes
-  def sha256(base : Bytes|String) : String
+  def sha256(base : Bytes | String) : String
     hash = OpenSSL::Digest.new("SHA256")
     hash.update(base)
     hash.hexdigest
   end
 
-  def ripemd160(base : Bytes|String) : String
+  def ripemd160(base : Bytes | String) : String
     hash = OpenSSL::Digest.new("RIPEMD160")
     hash.update(base)
     hash.hexdigest
