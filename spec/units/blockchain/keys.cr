cb4f881e63d65e98b8f47b348a78102322d94c06
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-02-04 19:52:37 +0000

    wip - conversions between key formats and wif - for refactoring wallet to include wif, single public key and encrypted wallet

---
+++ spec/units/blockchain/keys.cr
@@ -0,0 +1,16 @@
+require "./../../spec_helper"
+require "./../utils"
+
+include Sushi::Core
+include Sushi::Core::Keys
+
+describe Keys do
+
+  describe PublicKey do
+
+   it "should create a public key object from a public key x and y" do
+   end
+
+  end
+
+end
---
+++ src/core/blockchain/keys.cr
@@ -0,0 +1,111 @@
+module ::Sushi::Core::Keys
+  extend Hashes
+
+  class PublicKey
+
+    def initalize(hex : String)
+      @hex = hex
+    end
+
+    def self.from(hex : String) : PublicKey
+      PublicKey.new(hex)
+    end
+
+    def self.from(bytes : Array(UInt8)) : PublicKey
+      PublicKey.new(to_hex(bytes))
+    end
+
+    def as_hex : String
+      @hex
+    end
+
+    def as_bytes : Array(UInt8)
+      to_bytes(@hex)
+    end
+
+    def address : String
+    end
+
+    def is_valid? : Bool
+    end
+
+  end
+
+  class PrivateKey
+
+    def initalize(hex : String)
+      @hex = hex
+    end
+
+    def self.from(hex : String) : PrivateKey
+      PrivateKey.new(hex)
+    end
+
+    def self.from(bytes : Array(UInt8)) : PrivateKey
+        PrivateKey.new(to_hex(bytes))
+    end
+
+    def as_hex : String
+      @hex
+    end
+
+    def as_bytes : Array(UInt8)
+      to_bytes(@hex)
+    end
+
+    def wif : Wif
+    end
+
+    def private_key : PrivateKey
+    end
+
+    def public_key : PublicKey
+    end
+
+    def address : String
+    end
+
+    def is_valid? : Bool
+    end
+
+  end
+
+
+  class Wif
+
+    def initialize(private_key : PrivateKey, network : Network)
+      @wif = to_wif(private_key, network)
+    end
+
+    def self.from(private_key : PrivateKey, network : Network) : Wif
+      Wif.new(private_key, network)
+    end
+
+    def self.from(wif : String) : Wif
+    end
+
+    def private_key : PrivateKey
+    end
+
+    def public_key : PublicKey
+    end
+
+    def network : Network
+    end
+
+    def address : String
+    end
+
+    private def to_wif(private_key : PrivateKey, network : Network) : Wif
+    end
+
+  end
+
+
+  def to_hex(bytes) : String
+  end
+
+  def to_bytes(hex) : Array(UInt8)
+  end
+
+end
