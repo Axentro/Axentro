e95f36cddd487c802188e5d4a25e45b8019f3aa4
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-02-07 11:52:59 +0000

    more specs

--- spec/units/blockchain/keys.cr
+++ spec/units/blockchain/keys.cr
@@ -5,6 +5,28 @@ include Sushi::Core
 include Sushi::Core::Keys
 
 describe Keys do
+  describe "#Keys.generate" do
+    it "should generate a private and public key pair as Key objects" do
+      keys = Keys.generate
+
+      keys.private_key.should be_a(PrivateKey)
+      keys.private_key.as_hex.size.should eq(64)
+
+      keys.public_key.should be_a(PublicKey)
+      keys.public_key.as_hex.size.should eq(128)
+    end
+
+    it "should generate a key pair for the mainnet when no network supplied" do
+      keys = Keys.generate
+      keys.public_key.network.should eq({prefix: "M0", name: "mainnet"})
+    end
+
+    it "should generate a key pair for the specified network" do
+      keys = Keys.generate({prefix: "T0", name: "testnet"})
+      keys.public_key.network.should eq({prefix: "T0", name: "testnet"})
+    end
+  end
+
   describe PublicKey do
     describe "#initialize" do
       it "should create a public key object from a public key string" do
@@ -16,32 +38,21 @@ describe Keys do
         public_key.as_hex.should eq(hex_public_key)
       end
 
-      it "should raise an error if the public key string is not a valid public key" do
+      it "should raise an error if the public key hex string is not a valid public key" do
+        expect_raises(Exception, "Invalid public key: 123") do
+          PublicKey.new("123")
+        end
       end
     end
 
     describe "#from hex" do
       it "should create a public key object from a public key string" do
         secp256k1 = ECDSA::Secp256k1.new
+        key_pair = secp256k1.create_key_pair
+        hex_public_key = key_pair[:public_key].x.to_s(16) + key_pair[:public_key].y.to_s(16)
 
-
-         loop do
-             key_pair = secp256k1.create_key_pair
-             one = key_pair[:public_key].x.to_s(16)
-             p two = key_pair[:public_key].y.to_s(16)
-             sk = key_pair[:secret_key].to_s(16)
-             # p sk.size
-             # p one.size
-             # p two.size
-            hex_public_key = one + two
-            if hex_public_key.hexbytes? == nil
-              puts "is not hex: #{hex_public_key}"
-              break
-            end
-         end
-
-        # public_key = PublicKey.from(hex_public_key)
-        # public_key.as_hex.should eq(hex_public_key)
+        public_key = PublicKey.from(hex_public_key)
+        public_key.as_hex.should eq(hex_public_key)
       end
     end
 
@@ -51,7 +62,7 @@ describe Keys do
         key_pair = secp256k1.create_key_pair
         hex_public_key = key_pair[:public_key].x.to_s(16) + key_pair[:public_key].y.to_s(16)
         hexbytes = hex_public_key.hexbytes
-        # p typeof(hexbytes)
+
         public_key = PublicKey.from(hexbytes)
         public_key.as_bytes.should eq(hexbytes)
         public_key.as_hex.should eq(hex_public_key)
@@ -61,18 +72,61 @@ describe Keys do
     end
 
     it "should convert a public key from hex to bytes with #as_bytes" do
+      secp256k1 = ECDSA::Secp256k1.new
+      key_pair = secp256k1.create_key_pair
+      hex_public_key = key_pair[:public_key].x.to_s(16) + key_pair[:public_key].y.to_s(16)
+      hexbytes = hex_public_key.hexbytes
+
+      public_key = PublicKey.from(hex_public_key)
+      public_key.as_bytes.should eq(hexbytes)
     end
 
     it "should convert a public key from bytes to hex with #as_hex" do
+      secp256k1 = ECDSA::Secp256k1.new
+      key_pair = secp256k1.create_key_pair
+      hex_public_key = key_pair[:public_key].x.to_s(16) + key_pair[:public_key].y.to_s(16)
+      hexbytes = hex_public_key.hexbytes
+
+      public_key = PublicKey.from(hexbytes)
+      public_key.as_hex.should eq(hex_public_key)
     end
 
-    it "should return the address with #address" do
+    describe "#network" do
+      it "should return the mainnet by default" do
+        Keys.generate.public_key.network.should eq({prefix: "M0", name: "mainnet"})
+      end
+      it "should return the supplied network" do
+        Keys.generate({prefix: "T0", name: "testnet"}).public_key.network.should eq({prefix: "T0", name: "testnet"})
+      end
+    end
+
+    describe "#address" do
+      it "should return the address" do
+        hex_public_key = "b9a152547ec31de50a726896293c7b99e63e6d9588b6d48fde5c926a1794d0616af3598724f335ec71b00509a69e3ce376a285ca5dd77ed5cce8e558c9d5b7e7"
+
+        public_key = PublicKey.from(hex_public_key)
+        public_key.address.should eq("TTA5YjVkYTA1NWVkNWQyNDYyMmNiMWU5M2EwZWVhZmFmODA4MDg3MjU5NzAxNTll")
+      end
+
+      it "should return a mainnet address" do
+        keys = Keys.generate
+        decoded_address = Base64.decode_string(keys.public_key.address)
+        decoded_address[0..1].should eq("M0")
+      end
+
+      it "should return a testnet address" do
+        keys = Keys.generate({prefix: "T0", name: "testnet"})
+        decoded_address = Base64.decode_string(keys.public_key.address)
+        decoded_address[0..1].should eq("T0")
+      end
     end
 
     describe "#is_valid?" do
       it "should return true if the public key is valid" do
-      end
-      it "should return false if the public key is invalid" do
+        # will always be true when called externally since we raise an error creating a public key with an invalid hex
+        # however when called internally we use this to determine if the supplied hex is valid when creating a public key
+        # so we can't test the false condition here.
+        Keys.generate.public_key.is_valid?.should be_true
       end
     end
   end
--- src/core/blockchain/keys.cr
+++ src/core/blockchain/keys.cr
@@ -1,18 +1,37 @@
 module ::Sushi::Core::Keys
-  extend Hashes
+  include Hashes
+  include Sushi::Core::Models
+
+  class Keys
+    property private_key : PrivateKey
+    property public_key : PublicKey
+
+    def initialize(@private_key : PrivateKey, @public_key : PublicKey)
+    end
+
+    def self.generate(network : Network = {prefix: "M0", name: "mainnet"})
+      secp256k1 = ECDSA::Secp256k1.new
+      key_pair = secp256k1.create_key_pair
+      private_key = PrivateKey.new(key_pair[:secret_key].to_s(16))
+      public_key = PublicKey.new(key_pair[:public_key].x.to_s(16) + key_pair[:public_key].y.to_s(16), network)
+      Keys.new(private_key, public_key)
+    end
+  end
 
   class PublicKey
+    getter network
 
-    def initialize(hex : String)
+    def initialize(hex : String, @network : Network = {prefix: "M0", name: "mainnet"})
       @hex = hex
+      raise "Invalid public key: #{@hex}" unless is_valid?
     end
 
-    def self.from(hex : String) : PublicKey
-      PublicKey.new(hex)
+    def self.from(hex : String, network : Network = {prefix: "M0", name: "mainnet"}) : PublicKey
+      PublicKey.new(hex, network)
     end
 
-    def self.from(bytes : Bytes) : PublicKey
-      PublicKey.new(to_hex(bytes))
+    def self.from(bytes : Bytes, network : Network = {prefix: "M0", name: "mainnet"}) : PublicKey
+      PublicKey.new(to_hex(bytes), network)
     end
 
     def as_hex : String
@@ -24,18 +43,19 @@ module ::Sushi::Core::Keys
     end
 
     def address : String
-      secp256k1 = ECDSA::Secp256k1.new
-      p secp256k1.create_key_pair(self.as_bytes)
-      ""
+      hashed_address = ripemd160(sha256(@hex))
+      network_address = @network[:prefix] + hashed_address
+      hashed_address_again = sha256(sha256(network_address))
+      checksum = hashed_address_again[0..5]
+      Base64.strict_encode(network_address + checksum)
     end
 
     def is_valid? : Bool
+      @hex.hexbytes? != nil
     end
-
   end
 
   class PrivateKey
-
     def initialize(hex : String)
       @hex = hex
     end
@@ -45,7 +65,7 @@ module ::Sushi::Core::Keys
     end
 
     def self.from(bytes : Bytes) : PrivateKey
-        PrivateKey.new(to_hex(bytes))
+      PrivateKey.new(to_hex(bytes))
     end
 
     def as_hex : String
@@ -70,12 +90,9 @@ module ::Sushi::Core::Keys
 
     def is_valid? : Bool
     end
-
   end
 
-
   class Wif
-
     def initialize(private_key : PrivateKey, network : Network)
       @wif = to_wif(private_key, network)
     end
@@ -101,9 +118,7 @@ module ::Sushi::Core::Keys
 
     private def to_wif(private_key : PrivateKey, network : Network) : Wif
     end
-
   end
-
 
   def to_hex(bytes : Bytes) : String
     bytes.to_unsafe.to_slice(bytes.size).hexstring
@@ -112,5 +127,4 @@ module ::Sushi::Core::Keys
   def to_bytes(hex : String) : Bytes
     hex.hexbytes
   end
-
 end
