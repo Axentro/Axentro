b85df3da8b26bcea8b529c0bc27ff5c0b560b0b6
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-02-07 12:03:50 +0000

    implementation of PrivateKey started

--- src/core/blockchain/keys.cr
+++ src/core/blockchain/keys.cr
@@ -19,7 +19,7 @@ module ::Sushi::Core::Keys
   end
 
   class PublicKey
-    getter network
+    getter network : Network
 
     def initialize(hex : String, @network : Network = {prefix: "M0", name: "mainnet"})
       @hex = hex
@@ -43,11 +43,7 @@ module ::Sushi::Core::Keys
     end
 
     def address : String
-      hashed_address = ripemd160(sha256(@hex))
-      network_address = @network[:prefix] + hashed_address
-      hashed_address_again = sha256(sha256(network_address))
-      checksum = hashed_address_again[0..5]
-      Base64.strict_encode(network_address + checksum)
+      get_address_from_public_key(self)
     end
 
     def is_valid? : Bool
@@ -56,16 +52,19 @@ module ::Sushi::Core::Keys
   end
 
   class PrivateKey
-    def initialize(hex : String)
+    getter network : Network
+
+    def initialize(hex : String, @network : Network = {prefix: "M0", name: "mainnet"})
       @hex = hex
+      raise "Invalid public key: #{@hex}" unless is_valid?
     end
 
-    def self.from(hex : String) : PrivateKey
-      PrivateKey.new(hex)
+    def self.from(hex : String, network : Network = {prefix: "M0", name: "mainnet"}) : PrivateKey
+      PrivateKey.new(hex, network)
     end
 
-    def self.from(bytes : Bytes) : PrivateKey
-      PrivateKey.new(to_hex(bytes))
+    def self.from(bytes : Bytes, network : Network = {prefix: "M0", name: "mainnet"}) : PrivateKey
+      PrivateKey.new(to_hex(bytes), network)
     end
 
     def as_hex : String
@@ -79,16 +78,21 @@ module ::Sushi::Core::Keys
     def wif : Wif
     end
 
-    def private_key : PrivateKey
-    end
-
     def public_key : PublicKey
+      secp256k1 = ECDSA::Secp256k1.new
+      key_pair = secp256k1.create_key_pair(@hex.to_big_i(16))
+
+      raise "Private key mismatch when finding public key" unless key_pair[:secret_key].to_s(16) == @hex
+
+      PublicKey.new(key_pair[:public_key].x.to_s(16) + key_pair[:public_key].y.to_s(16), @network)
     end
 
     def address : String
+      get_address_from_public_key(self.public_key)
     end
 
     def is_valid? : Bool
+      @hex.hexbytes? != nil
     end
   end
 
@@ -126,5 +130,13 @@ module ::Sushi::Core::Keys
 
   def to_bytes(hex : String) : Bytes
     hex.hexbytes
+  end
+
+  def get_address_from_public_key(public_key : PublicKey)
+    hashed_address = ripemd160(sha256(public_key.as_hex))
+    network_address = public_key.network[:prefix] + hashed_address
+    hashed_address_again = sha256(sha256(network_address))
+    checksum = hashed_address_again[0..5]
+    Base64.strict_encode(network_address + checksum)
   end
 end
