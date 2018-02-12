7dd7264b95b36eb4479dc6f968f1566f52a36999
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-02-12 15:58:34 +0000

    catch horrible errors and return human readable failure message

--- src/core/blockchain/wallet.cr
+++ src/core/blockchain/wallet.cr
@@ -39,14 +39,20 @@ module ::Sushi::Core
     end
 
     def self.decrypt(encrypted_wallet : EncryptedWallet, password) : Wallet
-      decrypted_wallet = BlowFish.decrypt(encrypted_wallet.cipher, password)
-      # TODO - veriy wallet before returning and handle any errors from decrypt
-      Wallet.from_json(decrypted_wallet)
+      begin
+        decrypted_wallet = BlowFish.decrypt(encrypted_wallet.cipher, password)
+      rescue ex
+        raise "Failed to decrypt wallet with supplied password"
+      end
+
+      wallet = Wallet.from_json(decrypted_wallet)
+      verify!(wallet.private_key, wallet.public_key, wallet.wif, wallet.address)
+      wallet
     end
 
     def self.create(password ,testnet = false) : EncryptedWallet
 
-      # raise "Password is too short, it must be a minimum of 16 characters" if password.size < 17
+      raise "Password is too short, it must be a minimum of 16 characters" if password.size < 16
 
       network = testnet ? TESTNET : MAINNET
       keys = Keys.generate(network)
@@ -65,27 +71,6 @@ module ::Sushi::Core
     def self.verify!(private_key : String, public_key : String, wif : String, address : String) : Bool
       Keys.is_valid?(private_key, public_key, wif, address)
     end
-
-    # def self.verify!(secret_key : String,
-    #                  public_key_x : String,
-    #                  public_key_y : String,
-    #                  address : String) : Bool
-    #   secp256k1 = ECDSA::Secp256k1.new
-    #   secret_key_raw = BigInt.new(Base64.decode_string(secret_key), base: 10)
-    #   public_key_raw_x = BigInt.new(Base64.decode_string(public_key_x), base: 10)
-    #   public_key_raw_y = BigInt.new(Base64.decode_string(public_key_y), base: 10)
-    #
-    #   raise "Invalid checksum for #{address}" unless valid_checksum?(address)
-    #
-    #   public_key = secp256k1.create_key_pair(secret_key_raw)[:public_key]
-    #   public_key_x = public_key.x.to_s(base: 10)
-    #   public_key_y = public_key.y.to_s(base: 10)
-    #
-    #   raise "Invalid public key (public_key_x) for #{public_key_raw_x} != #{public_key_x}" if public_key_raw_x.to_s != public_key_x
-    #   raise "Invalid public key (public_key_y) for #{public_key_raw_y} != #{public_key_y}" if public_key_raw_y.to_s != public_key_y
-    #
-    #   true
-    # end
 
     def self.address_network_type(address : String) : Models::Network
       Address.from(address).network
