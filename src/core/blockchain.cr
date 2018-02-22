5f62058c64a6a620fa321d7cd754e771a3c691ff
tbrand <taichiro0709@gmail.com>
2018-02-22 23:30:05 +0900

    revert

--- src/core/blockchain.cr
+++ src/core/blockchain.cr
@@ -237,7 +237,7 @@ module ::Sushi::Core
 
     def served_amount(index) : Int64
       total_fees = total_fees_of_latest_block
-      base = 40000
+      base = 10000
       div = (index / base).to_i
       return base.to_i64 + total_fees if div == 0
       (base / div).to_i64 + total_fees
