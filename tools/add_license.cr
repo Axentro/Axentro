659ee2227a16a4acf62cfd7d2908fc98c7a61483
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-04-03 16:47:53 +0100

    add license to all crystal files

---
+++ tools/add_license.cr
@@ -0,0 +1,37 @@
+# Copyright © 2017-2018 The SushiChain Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the SushiChain Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+LICENSE = <<-LIC
+# Copyright © 2017-2018 The SushiChain Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the SushiChain Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+LIC
+
+Dir["../**/*.cr"].reject{|f| f.starts_with?("../lib")}.each do |file_path|
+   lines = File.read_lines(file_path)
+   if lines.first.starts_with?("# Copyright")
+     puts "Good : #{file_path}"
+   else
+     puts "Amending : #{file_path}"
+     content = [LICENSE] + lines
+     File.open(file_path, "w"){|file| file.puts content.join("\n")}
+   end
+end
