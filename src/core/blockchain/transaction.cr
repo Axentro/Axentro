71cb0536c4e59eb042bda75b81b517d27b847a44
tbrand <taichiro0709@gmail.com>
2018-05-13 02:50:06 +0900

    [skip ci] fmt

--- src/core/blockchain/transaction.cr
+++ src/core/blockchain/transaction.cr
@@ -101,11 +101,11 @@ module ::Sushi::Core
         secp256k1 : ECDSA::Secp256k1 = ECDSA::Secp256k1.new
 
         raise "invalid signing" if !secp256k1.verify(
-            public_key.not_nil!.point,
-            self.as_unsigned.to_hash,
-            BigInt.new(@sign_r, base: 16),
-            BigInt.new(@sign_s, base: 16),
-          )
+                                     public_key.not_nil!.point,
+                                     self.as_unsigned.to_hash,
+                                     BigInt.new(@sign_r, base: 16),
+                                     BigInt.new(@sign_s, base: 16),
+                                   )
 
         blockchain.dapps.each do |dapp|
           dapp.valid?(self, transactions) if dapp.transaction_related?(@action)
