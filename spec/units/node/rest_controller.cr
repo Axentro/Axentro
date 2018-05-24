c214fc84e3c911f7e0bf8b7d92da1a96bd2f4090
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-05-24 10:22:03 +0100

    updates

--- spec/units/node/rest_controller.cr
+++ spec/units/node/rest_controller.cr
@@ -235,7 +235,219 @@ describe RESTController do
         end
       end
     end
+    it "should return zero amount when address is not found" do
+      with_factory do |block_factory, transaction_factory|
+        block_factory.addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_address(context("/api/v1/address/non-existing-address"), {address: "non-existing-address"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"]["confirmation"].should eq(1_i64)
+          result["result"]["pairs"][0].to_s.should eq("{\"token\" => \"SUSHI\", \"amount\" => \"0\"}")
+        end
+      end
+    end
   end
+
+  describe "__v1_address_token" do
+    it "should return the amounts for the specified address and token" do
+      with_factory do |block_factory, transaction_factory|
+        block_factory.addBlocks(2)
+        address = transaction_factory.sender_wallet.address
+        exec_rest_api(block_factory.rest.__v1_address_token(context("/api/v1/address/#{address}/token/SUSHI"), {address: address, token: "SUSHI"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"]["confirmation"].should eq(1_i64)
+          result["result"]["pairs"][0].to_s.should eq("{\"token\" => \"SUSHI\", \"amount\" => \"1.009253\"}")
+        end
+      end
+    end
+    it "should return no pairs when address and token is not found" do
+      with_factory do |block_factory, transaction_factory|
+        block_factory.addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_address_token(context("/api/v1/address/non-existing-address/token/NONE"), {address: "non-existing-address", token: "NONE"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"]["confirmation"].should eq(1_i64)
+          result["result"]["pairs"].to_s.should eq("[]")
+        end
+      end
+    end
+  end
+
+  describe "__v1_address_transactions" do
+    it "should return all transactions for the specified address" do
+      with_factory do |block_factory, transaction_factory|
+        address = transaction_factory.sender_wallet.address
+        transaction = transaction_factory.make_send(100_i64)
+        block_factory.addBlock([transaction]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_address_transactions(context("/api/v1/address/#{address}/transactions"), {address: address})) do |result|
+          result["status"].to_s.should eq("success")
+          Array(Transaction).from_json(result["result"].to_json)
+        end
+      end
+    end
+    it "should return filtered transactions for the specified address" do
+      with_factory do |block_factory, transaction_factory|
+        address = transaction_factory.sender_wallet.address
+        transaction = transaction_factory.make_send(100_i64)
+        block_factory.addBlock([transaction]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_address_transactions(context("/api/v1/address/#{address}/transactions?actions=send"), {address: address, actions: "send"})) do |result|
+          result["status"].to_s.should eq("success")
+          transactions = Array(Transaction).from_json(result["result"].to_json)
+          transactions.map { |txn| txn.action }.uniq.should eq(["send"])
+        end
+      end
+    end
+    it "should return empty list for filtered transactions for the specified address" do
+      with_factory do |block_factory, transaction_factory|
+        address = transaction_factory.sender_wallet.address
+        transaction = transaction_factory.make_send(100_i64)
+        block_factory.addBlock([transaction]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_address_transactions(context("/api/v1/address/#{address}/transactions?actions=unknown"), {address: address, actions: "unknown"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"].to_s.should eq("[]")
+        end
+      end
+    end
+    it "should return empty result when specified address and filter is not found" do
+      with_factory do |block_factory, transaction_factory|
+        address = transaction_factory.sender_wallet.address
+        transaction = transaction_factory.make_send(100_i64)
+        block_factory.addBlock([transaction]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_address_transactions(context("/api/v1/address/no-address/transactions?actions=unknown"), {address: "no-address", actions: "unknown"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"].to_s.should eq("[]")
+        end
+      end
+    end
+    it "should paginate default 20 transactions for the specified address" do
+      with_factory do |block_factory, transaction_factory|
+        address = transaction_factory.sender_wallet.address
+        block_factory.addBlocks(100)
+        exec_rest_api(block_factory.rest.__v1_address_transactions(context("/api/v1/address/#{address}/transactions"), {address: address})) do |result|
+          result["status"].to_s.should eq("success")
+          transactions = Array(Transaction).from_json(result["result"].to_json)
+          transactions.size.should eq(20)
+        end
+      end
+    end
+    it "should paginate transactions for the specified address" do
+      with_factory do |block_factory, transaction_factory|
+        address = transaction_factory.sender_wallet.address
+        block_factory.addBlocks(200)
+        exec_rest_api(block_factory.rest.__v1_address_transactions(context("/api/v1/address/#{address}/transactions?page_size=50&page=2"), {address: address, page_size: 50, page: 1})) do |result|
+          result["status"].to_s.should eq("success")
+          transactions = Array(Transaction).from_json(result["result"].to_json)
+          transactions.size.should eq(50)
+        end
+      end
+    end
+  end
+
+  describe "__v1_domain" do
+    it "should return the amounts for the specified domain" do
+      with_factory do |block_factory, transaction_factory|
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_domain(context("/api/v1/domain/#{domain}"), {domain: domain})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"]["confirmation"].should eq(1_i64)
+          result["result"]["pairs"][0].to_s.should eq("{\"token\" => \"SUSHI\", \"amount\" => \"1.3138795\"}")
+        end
+      end
+    end
+    it "should return error amount when domain is not found" do
+      with_factory do |block_factory, transaction_factory|
+        block_factory.addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_domain(context("/api/v1/domain/non-existing-domain"), {domain: "non-existing-domain"})) do |result|
+          result["status"].to_s.should eq("error")
+          result["reason"].should eq("the domain non-existing-domain is not resolved")
+        end
+      end
+    end
+  end
+
+  describe "__v1_domain_token" do
+    it "should return the amounts for the specified domain and token" do
+      with_factory do |block_factory, transaction_factory|
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_domain_token(context("/api/v1/domain/#{domain}/token/SUSHI"), {domain: domain, token: "SUSHI"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"]["confirmation"].should eq(1_i64)
+          result["result"]["pairs"][0].to_s.should eq("{\"token\" => \"SUSHI\", \"amount\" => \"1.3138795\"}")
+        end
+      end
+    end
+    it "should return no pairs when token is not found" do
+      with_factory do |block_factory, transaction_factory|
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_domain_token(context("/api/v1/address/#{domain}/token/NONE"), {domain: domain, token: "NONE"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"]["confirmation"].should eq(1_i64)
+          result["result"]["pairs"].to_s.should eq("[]")
+        end
+      end
+    end
+  end
+
+  describe "__v1_domain_transactions" do
+    it "should return all transactions for the specified domain" do
+      with_factory do |block_factory, transaction_factory|
+        transaction = transaction_factory.make_send(100_i64)
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction, transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_domain_transactions(context("/api/v1/domain/#{domain}/transactions"), {domain: domain})) do |result|
+          result["status"].to_s.should eq("success")
+          Array(Transaction).from_json(result["result"].to_json)
+        end
+      end
+    end
+    it "should return filtered transactions for the specified domain" do
+      with_factory do |block_factory, transaction_factory|
+        transaction = transaction_factory.make_send(100_i64)
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction, transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_domain_transactions(context("/api/v1/domain/#{domain}/transactions?actions=send"), {domain: domain, actions: "send"})) do |result|
+          result["status"].to_s.should eq("success")
+          transactions = Array(Transaction).from_json(result["result"].to_json)
+          transactions.map { |txn| txn.action }.uniq.should eq(["send"])
+        end
+      end
+    end
+    it "should return empty list for filtered transactions for the specified domain" do
+      with_factory do |block_factory, transaction_factory|
+        transaction = transaction_factory.make_send(100_i64)
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction, transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(2)
+        exec_rest_api(block_factory.rest.__v1_domain_transactions(context("/api/v1/domain/#{domain}/transactions?actions=unknown"), {domain: domain, actions: "unknown"})) do |result|
+          result["status"].to_s.should eq("success")
+          result["result"].to_s.should eq("[]")
+        end
+      end
+    end
+    it "should paginate default 20 transactions for the specified domain" do
+      with_factory do |block_factory, transaction_factory|
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(100)
+        exec_rest_api(block_factory.rest.__v1_domain_transactions(context("/api/v1/domain/#{domain}/transactions"), {domain: domain})) do |result|
+          result["status"].to_s.should eq("success")
+          transactions = Array(Transaction).from_json(result["result"].to_json)
+          transactions.size.should eq(20)
+        end
+      end
+    end
+    it "should paginate transactions for the specified domain" do
+      with_factory do |block_factory, transaction_factory|
+        domain = "sushi.sc"
+        block_factory.addBlock([transaction_factory.make_buy_domain_from_platform(domain, 0_i64)]).addBlocks(200)
+        exec_rest_api(block_factory.rest.__v1_domain_transactions(context("/api/v1/domain/#{domain}/transactions?page_size=50&page=2"), {domain: domain, page_size: 50, page: 1})) do |result|
+          result["status"].to_s.should eq("success")
+          transactions = Array(Transaction).from_json(result["result"].to_json)
+          transactions.size.should eq(50)
+        end
+      end
+    end
+  end
+
   STDERR.puts "< Node::RESTController"
 end
 
