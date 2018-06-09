57792b380a742bb490acea79f224907aa1d5590b
tbrand <taichiro0709@gmail.com>
2018-06-09 23:18:26 +0900

    [skip ci] client tokoroten

--- e2e/client.cr
+++ e2e/client.cr
@@ -14,13 +14,69 @@ require "random"
 require "./utils"
 
 module ::E2E
-  class Client
-    @transaction_ids = [] of String
-    @alive : Bool
+  class Client < Tokoroten::Worker
 
-    def initialize(@node_ports : Array(Int32), @num_miners : Int32)
-      @alive = true
+    @@client : Tokoroten::Worker? = nil
+
+    alias ClientWork = NamedTuple(call: Int32, content: String)
+
+    struct Initialize
+      JSON.mapping({node_ports: Array(Int32), num_miners: Int32})
     end
+
+    struct Result
+      JSON.mapping({num_transactions: Int32, duration: Float64})
+    end
+
+    def self.client
+      @@client.not_nil!
+    end
+
+    def self.initialize(node_ports : Array(Int32), num_miners : Int32)
+      @@client = Client.create(1)[0]
+
+      request = {call: 0, content: {node_ports: node_ports, num_miners: num_miners}.to_json}.to_json
+      client.exec(request)
+    end
+
+    def self.launch
+      request = {call: 1, content: ""}.to_json
+      client.exec(request)
+    end
+
+    def self.finish
+      request = {call: 2, content: ""}.to_json
+      client.exec(request)
+    end
+
+    def task(message : String)
+      work = ClientWork.from_json(message)
+
+      case work[:call]
+      when 0 # initialize
+        initialize = Initialize.from_json(work[:content])
+
+        @node_ports = initialize.node_ports
+        @num_miners = initialize.num_miners
+      when 1 # launch
+        launch
+      when 2 # finish
+        kill
+
+        response({num_transactions: num_transactions, duration: duration}.to_json)
+      end
+    end
+
+    def self.receive
+      client.receive
+    end
+
+    @transaction_ids = [] of String
+
+    @alive : Bool = true
+
+    @node_ports : Array(Int32) = [] of Int32
+    @num_miners : Int32 = 0
 
     def create_transaction
       sender = Random.rand(@num_miners)
