6dfce7142f8320cb997877c58fbc3f78c6182368
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-11-04 22:15:15 +0000

    small tweak

--- src/core/blockchain/consensus.cr
+++ src/core/blockchain/consensus.cr
@@ -57,8 +57,6 @@ module ::Sushi::Core::Consensus
     return ENV["SC_SET_DIFFICULTY"].to_i if ENV.has_key?("SC_SET_DIFFICULTY")
     return 1 if block_averages.size < 2
 
-    elapsed_block = timestamp - block.timestamp
-    block_averages.push(elapsed_block)
     block_averages = block_averages.select {|a| a > 0_i64}
     block_average = block_averages.reduce { |a, b| a + b } / block_averages.size
 
