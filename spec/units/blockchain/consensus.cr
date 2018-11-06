aeed1da699c2819dd816ee4819978acd83ac80c5
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-11-06 22:17:13 +0000

    fix specs

--- spec/units/blockchain/consensus.cr
+++ spec/units/blockchain/consensus.cr
@@ -65,19 +65,19 @@ describe Consensus do
       describe "when using average block time (after block averages are built)" do
         it "should maintain difficulty when average block time is within lower and upper bounds (10 secs -> 40 secs)" do
           block = Block.new(0_i64, [] of Transaction, 0_u64, "genesis", 0_i64, 20_i32)
-          block_times = (0..108).map { |n| 10_i64 }
+          block_times = (0..730).map { |n| 10_i64 }
           block_difficulty(100000_i64, 9_i64, block, block_times).should eq(20)
         end
 
         it "should raise difficulty when average block time is quicker than lower bounds (less than 10 secs on average)" do
           block = Block.new(0_i64, [] of Transaction, 0_u64, "genesis", 0_i64, 20_i32)
-          block_times = (0..108).map { |n| 9_i64 }
+          block_times = (0..730).map { |n| 9_i64 }
           block_difficulty(100000_i64, 11_i64, block, block_times).should eq(21)
         end
 
         it "should lower difficulty when average block time exceeds the upper bounds (greater than 40 secs on average)" do
           block = Block.new(0_i64, [] of Transaction, 0_u64, "genesis", 0_i64, 20_i32)
-          block_times = (0..108).map { |n| 100_i64 }
+          block_times = (0..730).map { |n| 100_i64 }
           block_difficulty(100000_i64, 9_i64, block, block_times).should eq(19)
         end
       end
