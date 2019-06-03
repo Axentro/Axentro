587d77c728063e4f36031fd4a6c09bb1560eb637
Raymond Barlow <rbarlow@raymanoz.com>
2019-06-03 21:19:49 +1000

    Fixed all Layout/TrailingWhitespace

--- src/core/modules/welcome.cr
+++ src/core/modules/welcome.cr
@@ -13,12 +13,12 @@
 module ::Sushi::Core::Welcome
   def logo
     plain = <<-LOGO
-.d8888. db    db .d8888. db   db d888888b  .o88b. db   db  .d8b.  d888888b d8b   db 
-88'  YP 88    88 88'  YP 88   88   `88'   d8P  Y8 88   88 d8' `8b   `88'   888o  88 
-`8bo.   88    88 `8bo.   88ooo88    88    8P      88ooo88 88ooo88    88    88V8o 88 
-  `Y8b. 88    88   `Y8b. 88~~~88    88    8b      88~~~88 88~~~88    88    88 V8o88 
-db   8D 88b  d88 db   8D 88   88   .88.   Y8b  d8 88   88 88   88   .88.   88  V888 
-`8888Y' ~Y8888P' `8888Y' YP   YP Y888888P  `Y88P' YP   YP YP   YP Y888888P VP   V8P 
+.d8888. db    db .d8888. db   db d888888b  .o88b. db   db  .d8b.  d888888b d8b   db
+88'  YP 88    88 88'  YP 88   88   `88'   d8P  Y8 88   88 d8' `8b   `88'   888o  88
+`8bo.   88    88 `8bo.   88ooo88    88    8P      88ooo88 88ooo88    88    88V8o 88
+  `Y8b. 88    88   `Y8b. 88~~~88    88    8b      88~~~88 88~~~88    88    88 V8o88
+db   8D 88b  d88 db   8D 88   88   .88.   Y8b  d8 88   88 88   88   .88.   88  V888
+`8888Y' ~Y8888P' `8888Y' YP   YP Y888888P  `Y88P' YP   YP YP   YP Y888888P VP   V8P
 LOGO
 
     light_green(plain)
