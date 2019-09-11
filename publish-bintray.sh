b6f46bd128842bf2d01ef0a7b5799f7fcb1349eb
Raymond Barlow <rbarlow@raymanoz.com>
2019-09-11 23:02:19 +1000

    Publish the latest successful build to bintray

---
+++ publish-bintray.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+version=0.0.${CIRCLE_BUILD_NUM}
+file=Sushi-${version}.tar.gz
+
+function create_package {
+  tar cvzf ${file} bin/sushi bin/sushid bin/sushim
+}
+
+function main {
+  credentials=raymanoz:${BINTRAY_API_KEY}
+  package=https://api.bintray.com/content/raymanoz/SushiChain/SushiChain
+
+  echo "Uploading ${version}..."
+  curl -T ${file} -u${credentials} ${package}/${version}/${file}
+
+  echo 
+  echo "Publishing ${version}..."
+  curl -X POST -u${credentials} ${package}/${version}/publish
+}
+
+create_package
+main
