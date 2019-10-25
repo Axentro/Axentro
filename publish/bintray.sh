59e36bca3ab80f57f0fe90703d005903a6f85e9f
Raymond Barlow <rbarlow@raymanoz.com>
2019-10-25 16:57:03 +1000

    Bintray publish (#329)
    * Another go at bintray publish (WIP)
    
    * install curl too
    
    * create artifact (to see if variable replacement is happening)
    
    * fixed variable name
    
    * trying another artefact option
    
    * trying another artefact option
    
    * Publish the latest successful build to bintray
    
    * Publish the latest successful build to bintray
    
    * attempting to publish to SushiChain org repo
    
    * attempting to publish to SushiChain org repo-take 2
    
    * Set version, build, publish
    
    * package up minimal files for a build from source
    
    * Streamlining--Don't need to build again for bintray publish.
    
    * Streamlining--Don't need to build again for bintray publish.
    
    * As requested in #329, e2e removed.
    brew will explicitly build only sushi, sushim, sushid.

---
+++ publish/bintray.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+version=$(publish/build_version.sh)
+file=Sushi-${version}.tar.gz
+
+function create_package {
+  tar cvzf "${file}" src dapps shard.yml version.txt
+}
+
+function main {
+  credentials=raymanoz:${BINTRAY_API_KEY}
+  package=https://api.bintray.com/content/sushichain/SushiChain/SushiChain
+
+  echo "Uploading ${version}..."
+  curl -T "${file}" -u"${credentials}" "${package}/${version}/${file}"
+
+  echo 
+  echo "Publishing ${version}..."
+  curl -X POST -u"${credentials}" "${package}/${version}/publish"
+}
+
+create_package
+main
