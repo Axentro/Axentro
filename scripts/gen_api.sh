92fbbbf2eb3c1a2285a7affb7dad690aa98a9967
Kingsley Hendrickse <khendrickse@kiwipowered.com>
2020-02-06 13:15:54 +0000

    move blueprints to docs

--- scripts/gen_api.sh
+++ scripts/gen_api.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-snowboard html -o src/core/node/virtual_file_system/api/v1/index.html -c src/core/node/virtual_file_system/api/v1/blueprints/config.yaml src/core/node/virtual_file_system/api/v1/blueprints/sushichain.apib
+snowboard html -o src/core/node/virtual_file_system/api/v1/index.html docs/api/v1/blueprints/sushichain.apib
