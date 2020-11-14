595519f8856ef3c16025929e03684d7cf4e41d78
Kingsley <kingsleyhendrickse@me.com>
2020-11-14 09:31:34 +0000

    burn token cli

--- src/cli/axe/token.cr
+++ src/cli/axe/token.cr
@@ -27,6 +27,10 @@ module ::Axentro::Interface::Axe
           desc: I18n.translate("axe.cli.token.lock.desc"),
         },
         {
+          name: I18n.translate("axe.cli.token.burn.title"),
+          desc: I18n.translate("axe.cli.token.burn.desc"),
+        },
+        {
           name: I18n.translate("axe.cli.token.list.title"),
           desc: I18n.translate("axe.cli.token.list.desc"),
         },
@@ -56,6 +60,8 @@ module ::Axentro::Interface::Axe
         return update
       when I18n.translate("axe.cli.token.lock.title")
         return lock
+      when I18n.translate("axe.cli.token.burn.title")
+        return burn  
       when I18n.translate("axe.cli.token.list.title")
         return list
       end
@@ -155,6 +161,37 @@ module ::Axentro::Interface::Axe
       kind = G.op.__is_fast_transaction ? TransactionKind::FAST : TransactionKind::SLOW
 
       add_transaction(node, "lock_token", [wallet], senders, recipients, "", token, kind)
+    end
+
+    def burn
+      puts_help(HELP_CONNECTING_NODE) unless node = G.op.__connect_node
+      puts_help(HELP_WALLET_PATH) unless wallet_path = G.op.__wallet_path
+      puts_help(HELP_FEE) unless fee = G.op.__fee
+      puts_help(HELP_AMOUNT) unless amount = G.op.__amount
+      puts_help(HELP_TOKEN) unless token = G.op.__token
+
+      raise "please specify your original token name" if token == TOKEN_DEFAULT
+
+      wallet = get_wallet(wallet_path, G.op.__wallet_password)
+
+      senders = SendersDecimal.new
+      senders.push({
+        address:    wallet.address,
+        public_key: wallet.public_key,
+        amount:     amount,
+        fee:        fee,
+        signature:  "0",
+      })
+
+      recipients = RecipientsDecimal.new
+      recipients.push({
+        address: wallet.address,
+        amount:  amount,
+      })
+
+      kind = G.op.__is_fast_transaction ? TransactionKind::FAST : TransactionKind::SLOW
+
+      add_transaction(node, "burn_token", [wallet], senders, recipients, "", token, kind)
     end
 
     def list
