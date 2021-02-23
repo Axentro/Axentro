e01decf5bf2940024d63997de8b286af22a3e8ab
Christian Kakesa <fenicks@users.noreply.github.com>
2021-02-23 09:34:58 +0100

    Build with crystal v0.36.1-1 (#400)

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -2,6 +2,7 @@
 
 # Build Axentro static binaries for GNU/Linux x86_64 via Docker
 
+CRYSTAL_VER="0.36.1-1"
 READLINK=`which readlink`
 
 MY_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
@@ -16,9 +17,9 @@ else
   git clone https://github.com/Axentro/Axentro.git
 fi
 
-if [ ! -d "crystal-0.35.1-1" ]; then
-  curl -L -o crystal-0.35.1-1-linux-x86_64.tar.gz https://github.com/crystal-lang/crystal/releases/download/0.35.1/crystal-0.35.1-1-linux-x86_64.tar.gz
-  tar -xzvf crystal-0.35.1-1-linux-x86_64.tar.gz
+if [ ! -d "crystal-${CRYSTAL_VER}" ]; then
+  curl -L -o crystal-${CRYSTAL_VER}-linux-x86_64.tar.gz https://github.com/crystal-lang/crystal/releases/download/${CRYSTAL_VER%%-*}/crystal-${CRYSTAL_VER}-linux-x86_64.tar.gz
+  tar -xzvf crystal-${CRYSTAL_VER}-linux-x86_64.tar.gz
 fi
 
 ########################
@@ -27,18 +28,18 @@ fi
 docker run --rm -it -v $PWD:/mnt ubuntu:16.04 /bin/bash -c "
 adduser --disabled-password --shell /bin/bash --gecos \"User\" $USER
 set -x ;
-export PATH=/mnt/crystal-0.35.1-1/bin:\$PATH ;
+export PATH=/mnt/crystal-${CRYSTAL_VER}/bin:\$PATH ;
 apt update -qq ;
 DEBIAN_FRONTEND=\"noninteractive\" TZ=\"Europe/London\" apt install -y curl libsqlite3-dev libevent-dev libpcre3-dev libssl-dev libxml2-dev libyaml-dev libgmp-dev libz-dev git build-essential ;
 cd /mnt/Axentro ;
-su $USER -c 'PATH=/mnt/crystal-0.35.1-1/bin:\$PATH shards install --production' ;
-su $USER -c 'PATH=/mnt/crystal-0.35.1-1/bin:\$PATH shards build --no-debug --static --release --production' ;
+su $USER -c 'PATH=/mnt/crystal-${CRYSTAL_VER}/bin:\$PATH shards install --production' ;
+su $USER -c 'PATH=/mnt/crystal-${CRYSTAL_VER}/bin:\$PATH shards build --no-debug --static --release --production' ;
 "
 
 cp Axentro/bin/axe* $BIN_DIR/
 test -d axentro-linux-x86_64 || mkdir axentro-linux-x86_64
 cp Axentro/bin/axe* axentro-linux-x86_64 && tar -cJf axentro-linux-x86_64.tar.xz axentro-linux-x86_64
 
-rm -rf Axentro axentro-linux-x86_64 crystal-0.35.1-1 crystal-0.35.1-1-linux-x86_64.tar.gz
+rm -rf Axentro axentro-linux-x86_64 crystal-${CRYSTAL_VER} crystal-${CRYSTAL_VER}-linux-x86_64.tar.gz
 
 cd "$OLDPWD"
